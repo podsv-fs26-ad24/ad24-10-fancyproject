83576990098f2a3e4c3d97eb1035b42d9302d47f
fankhjas <fankhjas@students.zhaw.ch>
2026-05-17 00:21:35 +0200

    data report update
    newly created files:
    data_report_new.qmd
    data_report_fancyproject.py
    data_report.html

---
+++ docs/data_report_fancyproject.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from ydata_profiling import ProfileReport
+
+df = pd.read_excel("traveldata-export.xlsx", sheet_name="travel_data")
+profile = ProfileReport(df, title="Travel Data Report",
+    type_schema={
+        "transport_mode": "text",
+        "departure_iata": "text",
+        "arrival_iata": "text",
+        "arrival_city": "text",
+        "arrival_country": "text",
+        "arrival_continent": "text",
+        "departure_city": "text",
+        "departure_country": "text",
+        "departure_continent": "text",
+        "aircraft_type": "text",
+        "business_unit": "text",
+        "subunit": "text",
+        "travel_purpose": "text",
+        "person_type": "text",
+        "haul": "text",
+        "travel_class": "text",
+    }
+)
+
+profile.to_file("data_report.html")
